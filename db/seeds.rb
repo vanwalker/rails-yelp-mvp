0db6947e7872a95ec1fcd5cb4094b5644f22f25f
Jonathan Leal <jonathanleal@msn.com>
2022-03-23 17:12:13 +0100

    Rails Yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant1 = Restaurant.create(name:"restaurant1", address:"restaurant1address", phone_number:"0606060606", category: "chinese")
+restaurant2 = Restaurant.create(name:"restaurant2", address:"restaurant2address", phone_number:"0606060606", category: "chinese")
+restaurant3 = Restaurant.create(name:"restaurant3", address:"restaurant3address", phone_number:"0606060606", category: "chinese")
+restaurant4 = Restaurant.create(name:"restaurant4", address:"restaurant4address", phone_number:"0606060606", category: "chinese")
+restaurant5 = Restaurant.create(name:"restaurant5", address:"restaurant5address", phone_number:"0606060606", category: "chinese")
